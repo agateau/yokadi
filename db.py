38baf1c02860dd86b52510d1e448579c8b0d6123
Aurélien Gâteau <aurelien@trinity.(none)>
2008-06-07 18:25:30 +0200

    Explicitly create the MTM table, to be able to assign an optional integer value.

--- db.py
+++ db.py
@@ -3,18 +3,39 @@ from sqlobject import *
 class Keyword(SQLObject):
     class sqlmeta:
         defaultOrder = "name"
-    name = UnicodeCol(alternateID=True)
-    tasks = RelatedJoin("Task")
+    name = UnicodeCol(alternateID=True, notNone=True)
+    tasks = RelatedJoin("Task",
+        createRelatedTable=False,
+        intermediateTable="task_keyword",
+        joinColumn="keyword_id",
+        otherColumn="task_id")
+
+class TaskKeyword(SQLObject):
+    task = ForeignKey("Task")
+    keyword = ForeignKey("Keyword")
+    value = IntCol(default=None)
 
 class Task(SQLObject):
     title = UnicodeCol(alternateID=True)
-    creationDate = DateTimeCol()
-    description = UnicodeCol()
-    urgency = IntCol()
+    creationDate = DateTimeCol(notNone=True)
+    description = UnicodeCol(default="", notNone=True)
+    urgency = IntCol(default=0, notNone=True)
     status = EnumCol(enumValues=['new', 'started', 'done'])
-    keywords = RelatedJoin("Keyword")
+    keywords = RelatedJoin("Keyword",
+        createRelatedTable=False,
+        intermediateTable="task_keyword",
+        joinColumn="task_id",
+        otherColumn="keyword_id")
+
+    def getKeywordDict(self):
+        dct = {}
+        for property in TaskKeyword.selectBy(task=self):
+            dct[property.keyword.name] = property.value
+        return dct
+
 
 def createTables():
     Keyword.createTable()
     Task.createTable()
+    TaskKeyword.createTable()
 # vi: ts=4 sw=4 et
