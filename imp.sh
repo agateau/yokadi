bce47023b7a2e4709c67bd96f8e43351e3910b37
Aurélien Gâteau <aurelien@trinity.(none)>
2008-07-05 00:59:45 +0200

    Useful scripts to import db.

---
+++ imp.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+set -e
+
+log() {
+	echo "#### $@"
+}
+
+cp ~/doc/todo.db test0.db
+
+log "Upgrading"
+python v1imp.py --db test0.db
+
+log "Dumping"
+sqlite3 test0.db <<EOF
+.output test.sql
+.dump
+EOF
+
+sed  '/CREATE TABLE/,/);/d' test.sql > test-nocreate.sql
+
+log "Creating empty db"
+rm -f test1.db
+python yokadi.py --db test1.db --create-only
+
+log "Restoring"
+sqlite3 test1.db <<EOF
+.read test-nocreate.sql
+EOF
+
+log "Done, new db is test1.db"
