d1e68b55fc953abf7b3b3d66f3f09af038796378
Sébastien Renard <Sebastien.Renard@digitalfox.org>
2008-08-29 23:03:48 +0200

    Keyword management : new functions to add and remove keywords

--- keywordcmd.py
+++ keywordcmd.py
@@ -6,6 +6,7 @@ Keyword related commands.
 @license: GPLv3
 """
 from db import *
+from utils import YokadiException
 
 
 class KeywordCmd(object):
@@ -14,3 +15,28 @@ class KeywordCmd(object):
         for keyword in Keyword.select():
             tasks=", ".join(str(task.id) for task in keyword.tasks)
             print "%s (tasks: %s)" % (keyword.name, tasks)
+
+    def do_k_add(self, line):
+        """Add a keyword"""
+        if not line:
+            raise YokadiException("You should provide at least a keyword name")
+        for keyword in line.split():
+            Keyword(name=keyword)
+            print "Keyword %s has been created" % keyword
+
+    def do_k_remove(self, line):
+        """Remove a keyword"""
+        keywords=Keyword.select(LIKE(Keyword.q.name, line))
+        if keywords.count()==0:
+            print "Sorry, no keyword name matching %s exists. Use k_list to see all defined keywords and k_add to create keywords" % line 
+            return
+        for keyword in keywords:
+            if keyword.tasks:
+                print "The keyword %s is used by the following tasks: %s" % (keyword.name,
+                                                                             ", ".join(str(task.id) for task in keyword.tasks))
+                answer=raw_input("Do you really want to remove this keyword ? (y/n)")
+                if answer!="y":
+                    print "Skipping deletion of keyword %s" % keyword.name
+                    continue
+            keyword.destroySelf()
+            print "Keyword %s has been removed" % keyword.name
