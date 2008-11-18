e9edffc25be00dc32d5cf9583255c162675c8c60
Aurélien Gâteau <aurelien@trinity.(none)>
2008-11-18 08:53:18 +0100

    Factorize line parsing and error handling.

--- projectcmd.py
+++ projectcmd.py
@@ -11,6 +11,22 @@ from completers import ProjectCompleter
 from utils import YokadiException
 from sqlobject import SQLObjectNotFound
 
+
+def projectByName(name, parameterName="project_name"):
+    """
+    Helper function which returns a project given its name, or raise a
+    YokadiException if it does not exist.
+    """
+    name = name.strip()
+    if len(name) == 0:
+        raise YokadiException("Missing <%s> parameter" % parameterName)
+
+    try:
+        return Project.byName(name)
+    except SQLObjectNotFound:
+        raise YokadiException("Project '%s' not found. Use p_list to see all projects." % name)
+
+
 class ProjectCmd(object):
     def do_p_rename(self, line):
         """Rename project.
@@ -21,7 +37,7 @@ class ProjectCmd(object):
         oldName = tokens[0]
         newName = tokens[1]
 
-        project = Project.selectBy(name=oldName)[0]
+        project = projectByName(oldName, "old_name")
         project.name = newName
         print "Renamed project '%s' to '%s'" % (oldName, newName)
     complete_p_rename = ProjectCompleter(1)
@@ -37,29 +53,18 @@ class ProjectCmd(object):
 
     def do_p_set_active(self, line):
         """Activate the given project"""
-        try:
-            Project.byName(line).active=True
-        except SQLObjectNotFound:
-            raise YokadiException("Project %s not found. Use p_list to see all projects" % line)
-
+        projectByName(line).active=True
     complete_p_set_active = ProjectCompleter(1)
 
     def do_p_set_inactive(self, line):
         """Desactivate the given project"""
-        try:
-            Project.byName(line).active=False
-        except SQLObjectNotFound:
-            raise YokadiException("Project %s not found. Use p_list to see all projects" % line)
-
+        projectByName(line).active=False
     complete_p_set_inactive = ProjectCompleter(1)
 
     def do_p_remove(self, line):
         """Remove a project and all its associated tasks
         p_remove <project_name>"""
-        line = line.strip()
-        if len(line) == 0:
-            raise YokadiException("You should give one argument: <project_name>")
-        project = Project.byName(line)
+        project = projectByName(line)
         taskList = Task.select(Task.q.projectID == project.id)
         taskList = list(taskList)
         print "Deleting project tasks:"
