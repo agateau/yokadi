a6a8b15b301abdabb8e54b4b69129999a1b9c9d8
Aurélien Gâteau <aurelien@trinity.(none)>
2008-11-20 09:50:04 +0100

    Move ParseUtilsTestCase to its own file

--- tests/tests.py
+++ tests/tests.py
@@ -7,45 +7,9 @@ Yokadi unit tests
 @license: GPLv3
 """
 
-import sys
-from os.path import dirname, join, pardir
 import unittest
 
-sys.path.append(join(dirname(sys.argv[0]), pardir))
-import parseutils
-
-gTaskLineToParsedStructList = [
-    (u"project some text -k keyword1 -k keyword2=12 some other text", (u"project", u"some text some other text", {u"keyword1":None, u"keyword2":12} )),
-    (u"project ééé", (u"project", u"ééé", {} )),
-    (u"project let's include quotes\"", (u"project", u"let's include quotes\"", {} )),
-    (u"   project this  one has  extra spaces  ", (u"project", u"this one has extra spaces", {} )),
-    ]
-
-class ParseUtilsTestCase(unittest.TestCase):
-    def testExtractKeywords(self):
-        for src, dst in gTaskLineToParsedStructList:
-            result = parseutils.parseTaskLine(src)
-            self.assertEqual(result, dst)
-
-    def testCreateTaskLine(self):
-        for dummy, parsedStruct in gTaskLineToParsedStructList:
-            # We do not check the result of createTaskLine() against the
-            # original task line because there are many ways to write the same
-            # taskLine.
-            taskLine = parseutils.createTaskLine(*parsedStruct)
-            result = parseutils.parseTaskLine(taskLine)
-            self.assertEqual(result, parsedStruct)
-
-
-    def testCompleteParameterPosition(self):
-        data = [
-                 (("bla", "t_add bla", 6, 10), 1),
-                 (("bli", "t_add bla bli", 10, 14), 2),
-               ]
-        for params, expectedResult in data:
-            result = parseutils.computeCompleteParameterPosition(*params)
-            self.assertEqual(result, expectedResult)
-
+from parseutilstestcase import ParseUtilsTestCase
 
 def main():
     testCases = [ParseUtilsTestCase]
