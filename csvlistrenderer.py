47c4ef393e9e58a12f2fb2c29a6f911c27b6d1cb
Aurélien Gâteau <aurelien@trinity.(none)>
2008-12-20 22:29:25 +0100

    Make sure all rows and all cells are correctly encoded.

--- csvlistrenderer.py
+++ csvlistrenderer.py
@@ -15,14 +15,17 @@ TASK_FIELDS = ["title", "creationDate", "dueDate", "doneDate", "description", "u
 class CsvListRenderer(object):
     def __init__(self, out):
         self.writer = csv.writer(out, dialect="excel")
-        self.writer.writerow(TASK_FIELDS) # Header
+        self._writerow(TASK_FIELDS) # Header
 
     def addTaskList(self, project, taskList):
         for task in taskList:
-            row = list(unicode(task.__getattribute__(field)).encode(tui.ENCODING) for field in TASK_FIELDS if field!="keywords")
+            row = [getattr(task, field) for field in TASK_FIELDS if field!="keywords"]
             row.append(task.getKeywordsAsString())
-            self.writer.writerow(row)
+            self._writerow(row)
 
     def end(self):
         pass
+
+    def _writerow(self, row):
+        self.writer.writerow([unicode(x).encode(tui.ENCODING) for x in row])
 # vi: ts=4 sw=4 et
