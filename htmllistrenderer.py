94c682d7454a0b9b0d5458c03b1f913f6a8fcd9d
Aurélien Gâteau <aurelien@trinity.(none)>
2008-12-20 22:21:09 +0100

    Prettify html output a bit.

--- htmllistrenderer.py
+++ htmllistrenderer.py
@@ -5,32 +5,44 @@ HTML rendering of t_list output
 @author: Aurélien Gâteau <aurelien.gateau@free.fr>
 @license: GPLv3
 """
-TASK_FIELDS = ["title", "creationDate", "dueDate", "doneDate", "description", "urgency", "status", "project", "keywords"]
+TASK_FIELDS = ["title", "creationDate", "dueDate", "doneDate", "description", "urgency", "status", "keywords"]
+
+def printRow(out, tag, lst):
+    print >>out, "<tr>"
+    for value in lst:
+        text = unicode(value).encode("utf-8") or "&nbsp;"
+        print >>out, "<%s>%s</%s>" % (tag, text, tag)
+    print >>out, "</tr>"
 
 class HtmlListRenderer(object):
     def __init__(self, out):
         self.out = out
 
         #TODO: make this fancier
-        self.out.write("""<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+        print >>self.out, """<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
                 <html>
                 <head>
+                    <style>
+                    td, th {
+                        border: 1px solid #ccc;
+                    }
+                    </style>
                     <title>Yokadi tasks export</title>
                     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
                 </head>
                 <body>
-                <table>
-                """)
+                """
 
     def addTaskList(self, project, taskList):
+        print >>self.out, (u"<h1>%s</h1>" % project.name).encode("utf-8")
+        print >>self.out, "<table width='100%'>"
+        printRow(self.out, "th", TASK_FIELDS)
         for task in taskList:
-            self.out.write("<tr>\n")
-            row = ["<td>%s</td>" % unicode(getattr(task, field)) for field in TASK_FIELDS if field!="keywords"]
-            row.append("<td>%s</td>" % task.getKeywordsAsString())
-            line=u" ".join(row)
-            self.out.write(line.encode("utf-8"))
-            self.out.write("</tr>\n")
+            lst = [getattr(task, field) for field in TASK_FIELDS if field!="keywords"]
+            lst.append(task.getKeywordsAsString())
+            printRow(self.out, "td", lst)
+        print >>self.out, "</table>"
 
     def end(self):
-        self.out.write("</table></body></html>\n")
+        print >>self.out, "</body></html>"
 # vi: ts=4 sw=4 et
