52322d13a739c8738988fbbfb0b66035bf4ca905
Aurélien Gâteau <aurelien.gateau@free.fr>
2009-02-06 21:30:02 +0100

    Added crude setup.py script.

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from distutils.core import setup
+
+setup(
+    name='yokadi',
+    version='0.9.0',
+    description='Command-line, sqlite-powered, TODO list',
+    author='Aurélien Gâteau',
+    author_email='aurelien.gateau@free.fr',
+    url='http://yokadi.github.com',
+    )
+
+# vi: ts=4 sw=4 et
