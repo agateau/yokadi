af8347939047aaf7420658192440b754f50ea65e
Sébastien Renard <Sebastien.Renard@digitalfox.org>
2009-02-09 21:27:04 +0100

    Don't use colors when stdout is not a tty

--- colors.py
+++ colors.py
@@ -5,12 +5,24 @@ Standard codes for shell colors.
 @author: Sébastien Renard <sebastien.renard@digitalfox.org>
 @license: GPLv3
 """
-    
-BOLD     = '\033[01m'
-RED     = '\033[31m'
-GREEN     = '\033[32m'
-ORANGE     = '\033[33m'
-PURPLE     = '\033[35m'
-CYAN    = '\033[36m'
-GREY    = '\033[37m'
-RESET     = '\033[0;0m'
+
+import sys
+
+if sys.stdout.isatty():
+    BOLD    = '\033[01m'
+    RED     = '\033[31m'
+    GREEN   = '\033[32m'
+    ORANGE  = '\033[33m'
+    PURPLE  = '\033[35m'
+    CYAN    = '\033[36m'
+    GREY    = '\033[37m'
+    RESET   = '\033[0;0m'
+else:
+    BOLD    = ''
+    RED     = ''
+    GREEN   = ''
+    ORANGE  = ''
+    PURPLE  = ''
+    CYAN    = ''
+    GREY    = ''
+    RESET   = ''
