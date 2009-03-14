5b708ae6ef84308a340e0c6e3cff1165188fb00f
Sébastien Renard <Sebastien.Renard@digitalfox.org>
2009-03-14 23:25:49 +0100

    Also add a wrapper for yokadi daemon on PATH

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ data_files.append(["share/yokadi",
                    ["version", "README.markdown", "ChangeLog", "ChangeLog-Synthesis", "LICENSE"]])
 
 # Scripts
-scripts=["src/bin/yokadi"]
+scripts=["src/bin/yokadi", "src/bin/yokadid"]
 
 # Version
 try:
