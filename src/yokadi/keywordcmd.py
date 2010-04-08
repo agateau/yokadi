8e9a96603221e5f6fb7bb488ecfe4db11e256191
Aurelien Gateau <agateau@kde.org>
2010-04-08 17:27:17 +0200

    Make use of tui, simplified k_remove (no need to support removing keywords by matching imo), documented k_add and k_remove

--- src/yokadi/keywordcmd.py
+++ src/yokadi/keywordcmd.py
@@ -5,6 +5,8 @@ Keyword related commands.
 @author: Aurélien Gâteau <aurelien.gateau@free.fr>
 @license: GPL v3 or later
 """
+import tui
+
 from db import Keyword
 from yokadiexception import YokadiException, BadUsageException
 from completers import KeywordCompleter
@@ -19,9 +21,10 @@ class KeywordCmd(object):
             print "%s (tasks: %s)" % (keyword.name, tasks)
 
     def do_k_add(self, line):
-        """Add a keyword"""
+        """Add a keyword
+        k_add @<keyword1> [@<keyword2>...]"""
         if not line:
-            raise BadUsageException("You should provide at least a keyword name")
+            raise BadUsageException("You must provide at least one keyword name")
         for keyword in line.split():
             try:
                 Keyword(name=keyword)
@@ -30,20 +33,23 @@ class KeywordCmd(object):
                 print "Keyword %s already exist" % keyword
 
     def do_k_remove(self, line):
-        """Remove a keyword"""
-        keywords = Keyword.select(LIKE(Keyword.q.name, line))
-        if keywords.count() == 0:
-            print "Sorry, no keyword name matching %s exists. Use k_list to see all defined keywords and k_add to create keywords" % line
+        """Remove a keyword
+        k_remove @<keyword>"""
+        if not line:
+            raise BadUsageException("You must provide a keyword name")
+        if line.startswith("@"):
+            line = line[1:]
+        lst = list(Keyword.selectBy(name=line))
+        if len(lst) == 0:
+            tui.error("Could not find any keyword named %s" % line)
             return
-        for keyword in keywords:
-            if keyword.tasks:
-                print "The keyword %s is used by the following tasks: %s" % (keyword.name,
-                                                                             ", ".join(str(task.id) for task in keyword.tasks))
-                answer = raw_input("Do you really want to remove this keyword ? (y/n)")
-                if answer != "y":
-                    print "Skipping deletion of keyword %s" % keyword.name
-                    continue
-            keyword.destroySelf()
-            print "Keyword %s has been removed" % keyword.name
+        keyword = lst[0]
+
+        if keyword.tasks:
+            print "The keyword %s is used by the following tasks: %s" % (keyword.name,
+                                                                         ", ".join(str(task.id) for task in keyword.tasks))
+            if tui.confirm("Do you really want to remove this keyword"):
+                keyword.destroySelf()
+                print "Keyword %s has been removed" % keyword.name
 
     complete_k_remove = KeywordCompleter(1)
