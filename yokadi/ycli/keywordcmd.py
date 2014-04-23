1bffe06f32a0ac87caf1f0631149edda580b9030
Sébastien Renard <Sebastien.Renard@digitalfox.org>
2014-04-23 23:55:10 +0200

    Convert KeywordCmd to sql alchemy

--- yokadi/ycli/keywordcmd.py
+++ yokadi/ycli/keywordcmd.py
@@ -3,12 +3,15 @@
 Keyword related commands.
 
 @author: Aurélien Gâteau <aurelien.gateau@free.fr>
+@author: Sébastien Renard <sebastien.renard@digitalfox.org>
 @license: GPL v3 or later
 """
+from sqlalchemy.exc import IntegrityError
+
 from yokadi.core import dbutils
 from yokadi.ycli import tui
 
-from yokadi.core.db import Keyword
+from yokadi.core.db import Keyword, DBHandler
 from yokadi.core.yokadiexception import BadUsageException
 from yokadi.ycli.completers import KeywordCompleter
 
@@ -16,32 +19,37 @@ from yokadi.ycli.completers import KeywordCompleter
 class KeywordCmd(object):
     def do_k_list(self, line):
         """List all keywords."""
-        for keyword in Keyword.select():
+        for keyword in DBHandler.getSession().query(Keyword).all():
             tasks = ", ".join(str(task.id) for task in keyword.tasks)
             print "%s (tasks: %s)" % (keyword.name, tasks)
 
     def do_k_add(self, line):
         """Add a keyword
         k_add @<keyword1> [@<keyword2>...]"""
+        session = DBHandler.getSession()
         if not line:
             raise BadUsageException("You must provide at least one keyword name")
         for keyword in line.split():
             try:
-                Keyword(name=keyword)
+                session.add(Keyword(name=keyword))
+                session.commit()
                 print "Keyword %s has been created" % keyword
-            except DuplicateEntryError:
+            except IntegrityError:
+                session.rollback()
                 print "Keyword %s already exist" % keyword
 
     def do_k_remove(self, line):
         """Remove a keyword
         k_remove @<keyword>"""
+        session = DBHandler.getSession()
         keyword = dbutils.getKeywordFromName(line)
 
         if keyword.tasks:
             print "The keyword %s is used by the following tasks: %s" % (keyword.name,
                                                                          ", ".join(str(task.id) for task in keyword.tasks))
             if tui.confirm("Do you really want to remove this keyword"):
-                keyword.destroySelf()
+                session.delete(keyword)
+                session.commit()
                 print "Keyword %s has been removed" % keyword.name
 
     complete_k_remove = KeywordCompleter(1)
@@ -49,6 +57,7 @@ class KeywordCmd(object):
     def do_k_edit(self, line):
         """Edit a keyword
         k_edit @<keyword>"""
+        session = DBHandler.getSession()
         keyword = dbutils.getKeywordFromName(line)
         oldName = keyword.name
         newName = tui.editLine(oldName)
@@ -56,10 +65,12 @@ class KeywordCmd(object):
             print "Cancelled"
             return
 
-        lst = list(Keyword.selectBy(name=newName))
+        lst = session.query(Keyword).filter_by(name=newName).all()
         if len(lst) == 0:
             # Simple case: newName does not exist, just rename the existing keyword
             keyword.name = newName
+            session.merge(keyword)
+            session.commit()
             print "Keyword %s has been renamed to %s" % (oldName, newName)
             return
 
@@ -70,7 +81,7 @@ class KeywordCmd(object):
 
         # Check we can merge
         conflictingTasks = []
-        for task in keyword.tasks:
+        for task in keyword.getTasks():
             kwDict = task.getKeywordDict()
             if oldName in kwDict and newName in kwDict and kwDict[oldName] != kwDict[newName]:
                 conflictingTasks.append(task)
@@ -84,13 +95,14 @@ class KeywordCmd(object):
             return
 
         # Merge
-        for task in keyword.tasks:
+        for task in keyword.getTasks():
             kwDict = task.getKeywordDict()
             if not newName in kwDict:
                 kwDict[newName] = kwDict[oldName]
             del kwDict[oldName]
             task.setKeywordDict(kwDict)
-        keyword.destroySelf()
+        session.delete(keyword)
+        session.commit()
         print "Keyword %s has been merged with %s" % (oldName, newName)
 
     complete_k_edit = KeywordCompleter(1)
