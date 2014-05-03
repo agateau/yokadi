c3a21e2900d6ac38961973f24edb184bfd377fe4
Aurélien Gâteau <agateau@kde.org>
2014-05-03 22:11:23 +0200

    Remove ysc, yss and yw for now

--- yokadi/yss/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Yokadi sync server package
-@author: Aurélien Gâteau <aurelien.gateau@free.fr>
-@author: Sébastien Renard <sebastien.renard@digitalfox.org>
-@license:GPL v3 or later
-"""
