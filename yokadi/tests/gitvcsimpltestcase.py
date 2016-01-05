d51ac6e44429ca89268936ab536e8a0c381d5dc0
Aurélien Gâteau <mail@agateau.com>
2016-01-05 07:57:43 +0100

    Add tests for init, isWorkTreeClean, commitAll, getStatus

--- yokadi/tests/gitvcsimpltestcase.py
+++ yokadi/tests/gitvcsimpltestcase.py
@@ -7,9 +7,22 @@ from tempfile import TemporaryDirectory
 
 from yokadi.sync.gitvcsimpl import GitVcsImpl
 
+
 def createGitRepository(path):
     os.mkdir(path)
     subprocess.check_call(('git', 'init', '--quiet'), cwd=path)
+
+
+def gitAdd(path):
+    dirname = os.path.dirname(path)
+    basename = os.path.basename(path)
+    subprocess.check_call(('git', 'add', basename), cwd=dirname)
+
+
+def touch(dirname, name):
+    path = join(dirname, name)
+    open(path, "w").close()
+    return path
 
 
 class GitVcsImplTestCase(unittest.TestCase):
@@ -24,3 +37,65 @@ class GitVcsImplTestCase(unittest.TestCase):
 
             impl.setDir(tmpDir)
             self.assertFalse(impl.isValidVcsDir())
+
+    def testInit(self):
+        with TemporaryDirectory() as tmpDir:
+            repoDir = join(tmpDir, "repo")
+            os.mkdir(repoDir)
+
+            impl = GitVcsImpl()
+            impl.setDir(repoDir)
+            impl.init()
+
+            gitDir = join(repoDir, ".git")
+            self.assertTrue(os.path.exists(gitDir))
+
+    def testIsWorkTreeClean(self):
+        with TemporaryDirectory() as tmpDir:
+            repoDir = join(tmpDir, "repo")
+            createGitRepository(repoDir)
+
+            impl = GitVcsImpl()
+            impl.setDir(repoDir)
+
+            self.assertTrue(impl.isWorkTreeClean())
+
+            touch(repoDir, "foo")
+            self.assertFalse(impl.isWorkTreeClean())
+
+    def testCommitAll(self):
+        with TemporaryDirectory() as tmpDir:
+            repoDir = join(tmpDir, "repo")
+            createGitRepository(repoDir)
+            touch(repoDir, "foo")
+
+            impl = GitVcsImpl()
+            impl.setDir(repoDir)
+
+            self.assertFalse(impl.isWorkTreeClean())
+            impl.commitAll()
+            self.assertTrue(impl.isWorkTreeClean())
+
+    def testGetStatus(self):
+        with TemporaryDirectory() as tmpDir:
+            repoDir = join(tmpDir, "repo")
+            createGitRepository(repoDir)
+            impl = GitVcsImpl()
+            impl.setDir(repoDir)
+
+            removed = touch(repoDir, "removed")
+            impl.commitAll()
+
+            touch(repoDir, "unknown")
+
+            added = touch(repoDir, "added")
+            gitAdd(added)
+
+            os.unlink(removed)
+
+            status = set(impl.getStatus())
+
+            self.assertTrue((b' D', b'removed') in status)
+            self.assertTrue((b'??', b'unknown') in status)
+            self.assertTrue((b'A ', b'added') in status)
+            self.assertEqual(len(status), 3)
