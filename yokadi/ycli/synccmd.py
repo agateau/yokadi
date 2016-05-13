2334085928faf6a2adbcd4c2eb8c00d68a03590e
Aurélien Gâteau <mail@agateau.com>
2016-05-13 19:54:28 +0200

    Rework sync commands
    - Hide some plumbing commands (prefixing them with _s)
    - Document them
    - Add --clear option to _s_dump
    - Add _s_import
    - Move the --all option from _s_pull to _s_import

--- yokadi/ycli/synccmd.py
+++ yokadi/ycli/synccmd.py
@@ -96,6 +96,8 @@ class SyncCmd(Cmd):
         self.syncManager = SyncManager(self.dumpDir)
 
     def do_s_sync(self, line):
+        """Synchronize the database with the remote one. Get the latest
+        changes, import them in the database and push local changes"""
         pullUi = TextPullUi()
 
         print("Dumping database")
@@ -125,38 +127,60 @@ class SyncCmd(Cmd):
         self._printPullResults(pullUi)
 
     def do_s_init(self, line):
+        """Create a dump directory."""
         self.syncManager.initDumpRepository()
         self.syncManager.dump()
-        print('Synchronization initialized, repository is in {}'.format(self.dumpDir))
+        print('Synchronization initialized, dump directory is in {}'.format(self.dumpDir))
 
-    def do_s_dump(self, line):
-        self.syncManager.dump()
-        print('Database dumped in {}'.format(self.dumpDir))
-
-    def do_s_pull(self, line):
-        parser = self.parser_s_pull()
+    def do__s_dump(self, line):
+        parser = self.parser__s_dump()
         args = parser.parse_args(line)
+        if args.clear:
+            self.syncManager.clearDump()
+        pullUi = TextPullUi()
+        self.syncManager.dump(pullUi)
+
+        print("Database dumped in {}".format(self.dumpDir))
+        self._printPullResults(pullUi)
+
+    def parser__s_dump(self):
+        parser = YokadiOptionParser()
+        parser.usage = "_s_dump [options]"
+        parser.description = "Dump database in the dump directory."
+        parser.add_argument("--clear", dest="clear", default=False, action="store_true",
+                            help="Clear the current dump before. This can be dangerous: any change present in the dump but not in the database will be lost.")
+        return parser
+
+    def do__s_pull(self, line):
+        """Pull the changes from a remote repository in the dump directory.
+        This command does *not* import the changes in the database. You need to call _s_import to do so."""
         pullUi = TextPullUi()
         self.syncManager.pull(pullUi=pullUi)
+        self._printPullResults(pullUi)
+
+    def do__s_import(self, line):
+        parser = self.parser__s_import()
+        args = parser.parse_args(line)
+        pullUi = TextPullUi()
         if args.all:
             self.syncManager.importAll(pullUi=pullUi)
         else:
             self.syncManager.importSinceLastSync(pullUi=pullUi)
-        self._printPullResults(pullUi)
 
-    def parser_s_pull(self):
+    def parser__s_import(self):
         parser = YokadiOptionParser()
-        parser.usage = "s_pull [options]"
-        parser.description = "Pull changes from a remote repository"
+        parser.usage = "_s_import [options]"
+        parser.description = "Import changes from the dump directory in the database."
         parser.add_argument("--all", dest="all", default=False, action="store_true",
                             help="Import all changes, regardless of the current synchronization status")
         return parser
 
     def do_s_push(self, line):
+        """Push changes from the dump directory to the remote repository."""
         try:
             self.syncManager.push()
         except NotFastForwardError:
-            print("Remote has other changes, you need to run s_pull")
+            print("Remote has other changes, you need to run _s_pull")
         except VcsImplError as exc:
             print("Failed to push: {}".format(exc))
 
